ce47f9cfebedffd0c2b404e12afa79957f5c2a0a
pixelgame239 <pixelgame2004@outlook.com.vn>
2026-03-30 21:35:05 +0700

    fix cron job

--- backend/src/main/java/com/photocopy/backend/controller/CronController.java
+++ backend/src/main/java/com/photocopy/backend/controller/CronController.java
@@ -1,12 +1,23 @@
 package com.photocopy.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class CronController {
+    private final JdbcTemplate jdbcTemplate;
     @GetMapping("/checkCron")
-    public String checkCron() {
-        return "Cron job is running";
+    public ResponseEntity<String> checkCron() {
+        try{
+            jdbcTemplate.execute("SELECT 1");
+            return ResponseEntity.ok("Cron job is running");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Cron job is not running: " + e.getMessage());
+        }
     }
 }
